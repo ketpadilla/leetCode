b4d66d04bcb4f2ca4cfc9d26d0dc460590663175
Kristian Padilla <ketpadilla05@gmail.com>
2024-01-02 23:15:25 +0800

    day1

---
+++ python/001-permutations.py
@@ -0,0 +1,34 @@
+## Problem: 46. Permutations
+from itertools import permutations 
+
+class Solution(object):
+  def permute(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+
+    # * Solution 1: Using itertools
+    # perm = permutations(nums)
+    # return print(list(perm))
+  
+    # * Solution 2: Backstracking
+    perm = list()
+    self.backtrack(nums, perm, [])
+    return print(perm)
+  
+  def backtrack(self, nums, perm, temp):
+    if len(temp) == len(nums):
+      perm.append(temp[:])
+    else:
+      for i in range(len(nums)):
+        if nums[i] in temp:
+          continue
+        temp.append(nums[i])
+        self.backtrack(nums, perm, temp)
+        temp.pop()
+    
+if __name__ == "__main__":
+    nums = [1, 2, 3]
+    Solution().permute(nums)
+
